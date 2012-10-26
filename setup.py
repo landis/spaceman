cce730105579fc82fb1e941a8c8b4e43264c2851
James Landritsi <james.landritsi@gmail.com>
2012-10-26 11:05:24 -0400

    still frameworking

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(    name = "spaceman",
+       version = "1.0",
+      packages = ['bmp085','hih4030','gps'],
+        author = "James Landritsi",
+  author_email = "james.landritsi@gmail.com",
+           url = "http://github.com/landis/spaceman"
+  )
